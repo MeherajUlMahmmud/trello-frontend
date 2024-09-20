45341a2c7e67066d47c33604fe7720121fffc667
Meheraj <57069206+MeherajUlMahmmud@users.noreply.github.com>
2024-09-20 19:14:47 +0600

    error page updated

--- src/pages/utility/ErrorPage.tsx
+++ src/pages/utility/ErrorPage.tsx
@@ -1,30 +1,18 @@
-import "../../styles/errorPage.scss";
-import { dashboardRoute } from "../../utils/app_routes";
+import { dashboardRoute } from "@/utils/app_routes";
 
 const ErrorPage = () => {
   return (
     <>
-      <div className="errorPage">
-        <h2 className="errorPage__title">
+      <div className="flex flex-col items-center justify-center h-screen">
+        <h2 className="text-3xl font-bold mb-4">
           <span>404</span> Page Not Found
         </h2>
-        <h2 className="errorPage__subtitle" style={{
-          fontSize: "1.5rem",
-          fontWeight: "normal",
-          marginTop: "1rem",
-          color: "red"
-        }}>
+        <h2 className="text-xl font-normal mt-4 text-red-500">
           Sorry, the page you are looking for does not exist.
         </h2>
-        <h3 className="errorPage__subtitle">
-          You can always go back to the <a href={dashboardRoute}
-            className="underline"
-            style={{
-              scale: "1.2",
-              fontWeight: "bold",
-              marginLeft: "0.1rem"
-            }}
-          >
+        <h3 className="text-xl font-normal mt-4">
+          You can always go back to the
+          <a href={dashboardRoute} className="underline font-bold ml-1 scale-110">
             Dashboard
           </a>.
         </h3>
