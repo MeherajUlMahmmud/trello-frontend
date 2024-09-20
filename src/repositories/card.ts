c819fbacfad4ee022bfaa86b4032a36b6b904b53
Meheraj <57069206+MeherajUlMahmmud@users.noreply.github.com>
2024-09-20 21:10:00 +0600

    Update card.ts

--- src/repositories/card.ts
+++ src/repositories/card.ts
@@ -1,6 +1,11 @@
 import { loadLocalStorage } from "../utils/persistLocalStorage";
 import { CARD_URL } from "../utils/urls";
-import { sendGetRequest } from "./apiHandler";
+import {
+	sendDeleteRequest,
+	sendGetRequest,
+	sendPatchRequest,
+	sendPostRequest,
+} from "./apiHandler";
 
 const tokens = loadLocalStorage("tokens");
 
@@ -15,6 +20,42 @@ export const cardRepository = {
 	},
 	getCard: async (id: string) => {
 		const res = await sendGetRequest(CARD_URL + `${id}/`, tokens.access);
+		return res;
+	},
+	createCard: async (data: any, accessToken: string) => {
+		const res = await sendPostRequest(
+			CARD_URL + "create/",
+			data,
+			accessToken
+		);
+		return res;
+	},
+	updateCard: async (id: string, data: any) => {
+		const res = await sendPatchRequest(
+			CARD_URL + `${id}/update/`,
+			data,
+			tokens.access
+		);
+		return res;
+	},
+	deleteCard: async (id: string, accessToken: string) => {
+		const res = await sendDeleteRequest(CARD_URL + `${id}/`, accessToken);
+		return res;
+	},
+	moveCard: async (
+		id: string,
+		destinationId: string,
+		sourceIndex: number,
+		accessToken: string
+	) => {
+		const res = await sendPatchRequest(
+			CARD_URL + `${id}/move/`,
+			{
+				destinationId,
+				sourceIndex,
+			},
+			accessToken
+		);
 		return res;
 	},
 };
