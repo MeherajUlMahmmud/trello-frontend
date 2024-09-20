3ae3684f2a10362f822307455ca6e51ecc4d7de1
Meheraj <57069206+MeherajUlMahmmud@users.noreply.github.com>
2024-09-20 17:47:55 +0600

    Update WorkspaceSidebar.tsx

--- src/components/Workspace/WorkspaceSidebar.tsx
+++ src/components/Workspace/WorkspaceSidebar.tsx
@@ -18,34 +18,34 @@ interface ProjectCardProps {
 
 const WorkspaceSidebar: React.FC<WorkspaceSidebarProps> = ({ workspace, projectList, selectedProjectId, showSidebar, setShowSidebar, setShowCreateProjectModal }) => {
 	return (
-		<div className='flex flex-col gap-2 w-[300px] h-screen bg-gray-800 p-5'>
-			<div className='flex justify-between items-center w-full'>
+		<div className='flex flex-col gap-2 w-[300px] h-screen'>
+			<div className='flex justify-between items-center w-full py-1 px-4'>
 				<div className='flex items-center gap-2'>
-					<img className='w-12 h-12' src='https://trello-logos.s3.amazonaws.com/c95e52bc93e8086fa1ab432d40ef5300/170.png' alt='workspace' />
+					<img className='w-10 h-10 object-cover ' src='https://trello-logos.s3.amazonaws.com/c95e52bc93e8086fa1ab432d40ef5300/170.png' alt='workspace' />
 					<p className='text-lg font-medium text-white'>
 						{workspace.title}
 					</p>
 				</div>
-				<div className='flex items-center justify-center text-white p-2 rounded-full cursor-pointer w-8 hover:bg-gray-500'
+				<div className='flex items-center justify-center text-white p-2 rounded-full cursor-pointer w-8 hover:bg-gray-700'
 					onClick={() => setShowSidebar(!showSidebar)}
 				>
 					<i className="fa-solid fa-chevron-left"></i>
 				</div>
 			</div>
-			<hr className='border-gray-500' />
+			<hr className='border-b border-gray-500 mx-3' />
 			<div className='flex flex-col gap-2 w-full'>
-				<div className='flex justify-between items-center w-full'>
+				<div className='flex justify-between items-center w-full px-4'>
 					<p className='text-sm font-medium text-white'>
 						Projects
 					</p>
 					<CustomButton
 						icon='fa-solid fa-plus'
 						type={ButtonType.Button}
-						className='bg-white text-black'
+						className='px-3 bg-white text-black hover:text-white'
 						onClick={() => setShowCreateProjectModal(true)}
 					/>
 				</div>
-				<div className='flex flex-col gap-3 w-full'>
+				<div className='flex flex-col w-full'>
 					{
 						projectList && projectList?.length > 0 && projectList.map((project: any) => (
 							<ProjectCard
@@ -66,11 +66,11 @@ const ProjectCard: React.FC<ProjectCardProps> = ({ project, isActive, workspaceI
 	return (
 		<a
 			href={`/workspace/${workspaceId}/${project.uuid}`}
-			className={`flex items-center gap-2 ${isActive ? 'active' : ''}`}>
-			<img className='w-8 h-8'
+			className={`flex items-center gap-2 py-2 px-4 text-white ${isActive ? 'bg-gray-700' : ''}`}>
+			<img className='w-10 h-7 object-cover'
 				src='https://trello-logos.s3.amazonaws.com/c95e52bc93e8086fa1ab432d40ef5300/170.png'
 				alt='workspace' />
-			<p className='text-md font-medium text-white'>
+			<p className='text-sm font-medium'>
 				{project.title.length > 20 ? project.title.slice(0, 20) + '...' : project.title}
 			</p>
 		</a>
